098bf33d572c944177282c5d6c29348303fab66a
steves3142 <steves3142@gmail.com>
2024-01-11 01:01:29 -0500

    importing react select, country-city-state package, and creating CityPicker module

--- stepzen-weather-app/app/page.tsx
+++ stepzen-weather-app/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CityPicker from "@/components/CityPicker";
 import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
@@ -14,7 +15,7 @@ export default function Home() {
         <Divider className="my-10" />
 
         <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E] rounded-md">
-          {/* <CityPicker /> */}
+          <CityPicker />
         </Card>
       </Card>
     </div>
---
+++ stepzen-weather-app/components/CityPicker.tsx
@@ -0,0 +1,57 @@
+"use client"; 
+
+import { Country, City } from "country-state-city"
+import { useState } from "react";
+import Select from "react-select"
+import { useRouter } from "next/navigation";
+
+type option = {
+    value: {
+        latitude: string;
+        longitude: string;
+        isoCode: string;
+    };
+    label: string;
+} | null; 
+
+type cityOption = {
+    value: {
+        latitude: string; 
+        longitude: string; 
+        countryCode: string; 
+        name: string; 
+        stateCode: string;
+    };
+    label: string;
+} | null; 
+
+const options = Country.getAllCountries().map((country) => ({
+    value: {
+      latitude: country.latitude,
+      longitude: country.longitude,
+      isoCode: country.isoCode,
+    },
+    label: country.name,
+  }));
+  
+function CityPicker() {
+    const [selectedCountry, setSelectedCountry] = useState<option>(null); 
+    const [selectedCity, setSelectedCity] = useState<cityOption>(null); 
+    const router = useRouter(); 
+
+    const handleSelectedCountry = (option: option) => {
+        setSelectedCountry(option); 
+        setSelectedCity(null);
+    }
+
+  return (
+    <div>
+        <Select
+        value={selectedCountry} 
+        onChange={handleSelectedCountry}
+        options={options} />
+    </div>
+  )
+}
+
+export default CityPicker;
