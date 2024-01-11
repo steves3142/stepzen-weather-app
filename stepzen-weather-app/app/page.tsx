804249dd85714746a38ab6e8e5bc679ee102ad2f
steves3142 <steves3142@gmail.com>
2024-01-11 00:33:15 -0500

    first commit

---
+++ stepzen-weather-app/app/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto bg-white rounded-md">
+        <Text className="text-6xl font-bold text-center mb-10 color text-gray-500">Weather AI</Text>
+        <Subtitle className="text-xl text-center text-gray-400">
+          Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 + More!
+        </Subtitle>
+
+        <Divider className="my-10" />
+
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E] rounded-md">
+          {/* <CityPicker /> */}
+        </Card>
+      </Card>
+    </div>
+  )
+}
